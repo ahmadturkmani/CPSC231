d007d23b7c18f54a830957554de7d4dd8d0103e0
Aelbannan <Ahmed3lbannan@gmail.com>
2013-10-01 21:19:47 -0600

    Yus

--- Battleship/Battleship33.py
+++ Battleship/Battleship33.py
@@ -24,7 +24,7 @@ def print_titlescreen():
     return;
 # end
 
-def print_vessel(name, x, y, direction):
+def print_vessel(name):
     print('Your ' + name + ' is located at (' + chr(x + ord('A')) + ', ' + str(y + 1) + ')');
     print('It is positioned ' + direction + 'y.');
     print();
@@ -34,9 +34,9 @@ def print_vessel(name, x, y, direction):
 def get_location(name, spaces):
     global x; # this function can edit x
     global y; # this function can edit y
-    print ('Where would you like to place your ' + name + ' (' + str(spaces) + 'spaces)');
-    x = ord(input('Enter horizontal position (A-J):\t')) - ord('A');
-    y = (int(input('Enter vertical position (1-10):\t\t')) - 1);
+    print ('Where would you like to place your ' + name + ' (' + str(spaces) + ' spaces)');
+    x = input('Enter horizontal position (A-J):');
+    y = input('Enter vertical position (1-10) :');
     print();
     return;
 # end
@@ -45,7 +45,7 @@ def get_location(name, spaces):
 def modify_row(name, size):
     global y; # this function can edit y
     y = (y + size) % 10;
-    print_vessel(name, x, y, direction);
+    print_vessel(name);
     return;
 # end
 
@@ -53,28 +53,53 @@ def modify_row(name, size):
 def modify_column(name, size):
     global x; # this function can edit x
     x = (x + size) % 10;
-    print_vessel(name, x, y, direction);
+    print_vessel(name);
     return;
 # end
 
+def validate_location(name, size, direction):
+	global x; # this function can edit x
+	global y; # this function can edit y
+	if (len(x) == 1) and (len(y) == 1):
+		x = (ord(x) - ord('A'));
+		y = (int(y) - 1);
+		if (direction == 'horizontal') and (x > -1) and (x < 10):
+			print_vessel('Aircraft Carrier');
+			print('The end of your ' + name + ' is located at (' + chr(((x + size - 1) % 10) + ord('A')) + ', ' + str(y + 1) + ')');
+			print();
+		elif direction == 'vertical' and (y > -1) and (y < 10):
+			print_vessel('Aircraft Carrier', x, y, direction);
+			print('The end of your ' + name + ' is located at (' + chr(x + ord('A')) + ', ' + str(((y + size - 1) % 10) + 1) + ')');
+			print();
+		else:
+			print('The location you entered is not on the board!');
+			get_location('Aircraft Carrier', 5);
+			validate_location(name, size, direction);
+	else:
+		print('Please enter a LETTER (between A-J) and a NUMBER (between 1-10!)');
+		get_location('Aircraft Carrier', 5);
+		validate_location(name, size, direction);
+	return;
+		
+
 def main():
-    global direction;  # this function can edit direction
+	global direction;  # this function can edit direction
         # prints name, location, and orientation of a vessel
-    print_titlescreen();
+	print_titlescreen();
         # Ask user to place aircraft carrier
-    get_location('Aircraft Carrier', 5);
-    direction = 'horizontal';
-    print_vessel('Aircraft Carrier', x, y, direction);
+	get_location('Aircraft Carrier', 5);
+	direction = 'horizontal';	
+	validate_location('Aircraft Carrier', 5, direction);
         # based on input, calculate the position of the battleship
-    direction = 'vertical';
-    modify_column('Battleship', 5);
+	direction = 'vertical';
+	modify_column('Battleship', 5);
         # now calculate position of sub
-    direction = 'horizontal';
-    modify_row('Submarine', 4);
+	direction = 'horizontal';
+	modify_row('Submarine', 4);
         # the same with the destroyer
-    direction = 'vertical';
-    modify_column('Destroyer', 3);
-    return;
+	direction = 'vertical';
+	modify_column('Destroyer', 3);
+	return;
 # end
 
 main(); 
