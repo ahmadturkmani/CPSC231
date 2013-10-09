f14c947620a63b361449aed16f5d46b058de4fd1
Hilmi A. <2ndgenairborne@gmail.com>
2013-10-08 23:07:01 -0600

    Creation Complete

---
+++ Battleship/Hilmi/Set4_Ex2.py
@@ -0,0 +1,96 @@
+#********************************************
+# By Hilmi Abou-Saleh
+# October, 7 2013
+# Battleship CPSC231 - Brown Edition
+#********************************************
+
+# global variables
+VESSEL_NAMES = ["Aircraft Carrier", "Battleship", "Submarine", "Destroyer", "Patrol Boat"]
+VESSEL_SIZES = [5, 4, 3, 3, 2]
+
+# Title Screen! Now a function!
+def print_titlescreen():
+    print();
+    print('________________________________________');
+    print('|*********WELCOME TO BATTLESHIP!*******|');
+    print('|**************************************|');
+    print('|**************************************|');
+    print('|*********  The Game For All!  ********|');
+    print('|**************************************|');
+    print('|**************************************|');
+    print('|**************************************|');
+    print();
+    
+# end
+
+def print_vessel(x, y, direction, vessel_index):
+	name = VESSEL_NAMES[vessel_index]
+	print('Your ' + name + ' is located at (' + chr(x + ord('A')) + ', ' + str(y + 1) + ')')
+	print('It is positioned ' + direction + 'ly.')
+	print() #empty line
+
+# end
+
+def get_location(vessel_index):
+    global x # this function can edit x
+    global y # this function can edit y
+    global direction
+    name = VESSEL_NAMES[vessel_index]
+    size = VESSEL_SIZES[vessel_index]
+    print ('Where would you like to place your ' + name + ' (' + str(size) + ' spaces)')
+    x = input('Enter horizontal position (A-J):')
+    y = input('Enter vertical position (1-10) :')
+    direction = input("Enter direction of vessel (Horizontal/Vertical): ")
+    print()
+	
+# end
+
+def validate_location(x, y, direction, vessel_index):
+	# check if string is a single letter or smaller than a three digit number, if not, asks for input again, then checks
+	name = VESSEL_NAMES[vessel_index]
+	size = VESSEL_SIZES[vessel_index]
+	
+	if (len(x) == 1) and (len(y) < 3):
+		x = (ord(x) - ord('A'))
+		y = (int(y) - 1)
+		# checks if location is on the board and if the ship will fit, if not asks for input again and checks again
+		if (direction == 'Horizontal') and (-1 < x < (10 - size)) and (-1 < y < 10):
+			print_vessel(x, y, direction, vessel_index)
+			print('The end of your ' + name + ' is located at (' + chr(((x + size - 1) % 10) + ord('A')) + ', ' + str(y + 1) + ')')
+			print()
+		elif direction == 'Vertical' and (-1 < y < (10 - size)) and (-1 < x < 10):  
+			print_vessel(x, y, direction, vessel_index)
+			print('The end of your ' + name + ' is located at (' + chr(x + ord('A')) + ', ' + str(((y + size - 1) % 10) + 1) + ')')
+			print()
+		else:
+			print('The location you entered is not on the board!')
+			get_location(index)
+			validate_location(name, size, direction, vessel_index)
+	else:
+		print('Please enter a LETTER (between A-J) and a NUMBER (between 1-10!)')
+		get_location(index)
+		validate_location(name, size, direction, vessel_index)
+
+
+#Enter quits the program when the user inputs q
+def program_stop():
+	quit = 'n'
+	while quit == 'n':
+		quit = input("Do you want to quit(y/n): ")	
+	
+
+def main():
+	print_titlescreen();
+	global index
+	index = 0
+	for index in range(len(VESSEL_NAMES)):
+		get_location(index)
+		validate_location(x, y, direction, index)
+		index = index + 1    	
+	
+	#Calling Program to stop program.
+	program_stop()
+	
+# end
+
+main()
