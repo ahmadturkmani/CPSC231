179b255089ce65dfa4cfd3434b232e753550801c
Aelbannan <Ahmed3lbannan@gmail.com>
2013-10-23 17:02:13 -0600

    Update battleship.py

--- Battleship/Ahmed/battleship.py
+++ Battleship/Ahmed/battleship.py
@@ -1,15 +1,26 @@
+#AHMED ELBANNAN
+#T2G5
 
 import grid
 import t2_g5_ai
 import human
 
-for i in range(grid.NUM_OF_VESSELS):
-	human.get_location(i)
-	t2_g5_ai.get_location(i)
-	grid.print_grid(t2_g5_ai.defend_grid)
-	grid.print_grid(human.defend_grid)
-
+def main():
 	
+	for i in range(grid.NUM_OF_VESSELS):
+		human.get_location(i)
+		t2_g5_ai.get_location(i)
+		grid.print_grid(t2_g5_ai.defend_grid)
+		grid.print_grid(human.defend_grid)
+
+	while not all_vessels_sunk():
+		row, col = human.get_choice()
+		grid.drop_bomb(human.attack_grid, t2_g5_ai.defend_grid, row, col)
+	
+		row, col = t2_g5_ai.get_choice()
+		grid.drop_bomb(t2_g5_ai.attack_grid, human.defend_grid, row, col)
+
+main()
 	
 	
 	
