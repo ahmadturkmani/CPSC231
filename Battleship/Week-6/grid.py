3d5ed03a34114a5d8a5fee3ff4e2d665acd06e4c
ahmad.turkmani <ahmadt1@live.ca>
2013-10-23 18:30:03 -0600

    Update grid.py

--- Battleship/Week-6/grid.py
+++ Battleship/Week-6/grid.py
@@ -54,6 +54,41 @@ def has_overlap(grid, index, row, col, direction):
 						return True;
 	
 	return False
+# check the location picked by ai or human is a hit or a miss and print 
+#a grid with an 'X' for a hit and an 'o' for a miss	
+def drop_bomb(grid_defend,grid_attack,col_bomb,row_bomb):
+	#if the location picked to bomb equal any of the VESSEL_SIZE its is a hit
+	if grid_attack[col_bomb][row_bomb] ==	VESSEL_SIZE[index]
+			grid_attack[col_bomb][row_bomb] = HIT
+			grid_defend[col_bomb][row_bomb] = HIT
+			print('HIT')
+	#the location is empty 	
+	else:
+			grid_attack[col_bomb][row_bomb] = MISS
+			grid_defend[col_bomb][row_bomb] = MISS 
+			print('MISS')
+	print(grid_defend)
+	print(grid_attack)
+	
 
-						
-# end
+#check to see if there is any ship left on the grid, if no ship left return True as sunk else return false						
+def all_ships_sunk(grid_defend):
+	# TEMP VARIABLES
+	check_column = 0
+	check_row =0
+	Sunk= False
+	if direction == 'h'
+            for check_column in range(col,col+VESSEL_SIZE[index]):
+                if grid[check_column][check_row] != 5 or 4 or 3 or 2 :
+                    return True
+					
+	elif direction == 'v':
+            for check_y in range(row,row+GRID_HEIGHT):
+                if grid[check_column][check_row] != 5 or 4 or 3 or 2:
+                    return True:
+	
+	else:
+		sunk=False
+	
+	return sunk
+
