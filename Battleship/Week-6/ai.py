b517267e2b8139eeecd304b807172fa050b8b0bb
Aelbannan <Ahmed3lbannan@gmail.com>
2013-10-23 18:32:34 -0600

    Update ai.py

--- Battleship/Week-6/ai.py
+++ Battleship/Week-6/ai.py
@@ -1,3 +1,4 @@
+
 import grid
 import random
 
@@ -27,3 +28,49 @@ def get_location(index):
 	else:
 		defend_grid = grid.add_vessel(defend_grid, index, row, col, dir)
 	
+
+       
+def find_ships():
+	
+	found_ships = []
+	
+	for i in range(GRID_HEIGHT):
+		
+		for j in range(GRID_WIDTH):
+			
+			if attack_grid[i][j] == grid.HIT:
+				found_ships.append([j,i])
+				
+	if len(found_ships) > 0			
+		analyse_grid(found_ships)
+	else:
+		row, col = random_move()
+		
+	
+def random_move():
+	
+	col = random.randint(0,grid.GRID_WIDTH - 1)
+	row = random.randint(0,grid.GRID_HEIGHT - 1)
+	
+	while attack_grid[row][col] != grid.B:
+		
+		col = random.randint(0,grid.GRID_WIDTH - 1)
+		row = random.randint(0,grid.GRID_HEIGHT - 1)
+		
+	return row, col
+	
+def analyse_grid(found_ships):
+	
+	for i in range(len(found_ships)):
+		
+		nearby_x =[]
+		col = found_ships[i][0]
+		row = found_ships[i][1]
+		
+		for j in range(4):
+			
+			if (col - j) > -1:
+				
+				if attack_grid[row][col - j] == grid.HIT:
+					nearby_x.append([grid.HIT, 'l'
+		
