243c93f316f3db9e17419beb63332e6054349eab
Hilmi A. <2ndgenairborne@gmail.com>
2013-10-24 11:01:40 -0600

    Update grid.py

--- Battleship/Week-6/grid.py
+++ Battleship/Week-6/grid.py
@@ -7,7 +7,8 @@ VESSEL_NAME = ['Aircraft Carrier', 'Battleship', 'Submarine', 'Destroyer', 'Patr
 VESSEL_SIZE = [5, 4, 3, 2, 2];
 NUM_OF_VESSELS = 5
 B = '~' # this is water
-
+HIT = 'X'
+MISS = 'O'
 	
 	
 def print_grid(grid):
@@ -56,39 +57,32 @@ def has_overlap(grid, index, row, col, direction):
 	return False
 # check the location picked by ai or human is a hit or a miss and print 
 #a grid with an 'X' for a hit and an 'o' for a miss	
-def drop_bomb(grid_defend,grid_attack,col_bomb,row_bomb):
+def drop_bomb(grid_defend,grid_attack,row_bomb,col_bomb):
 	#if the location picked to bomb equal any of the VESSEL_SIZE its is a hit
-	if grid_attack[col_bomb][row_bomb] ==	VESSEL_SIZE[index]
-			grid_attack[col_bomb][row_bomb] = HIT
-			grid_defend[col_bomb][row_bomb] = HIT
-			print('HIT')
+	hit = "False"
+	for index in range(len(VESSEL_SIZE)):
+		if grid_defend[row_bomb][col_bomb] == VESSEL_SIZE[index]:
+			grid_attack[row_bomb][col_bomb] = HIT
+			grid_defend[row_bomb][col_bomb] = HIT
+			print("hit")
+			hit = "True"
+				
 	#the location is empty 	
-	else:
-			grid_attack[col_bomb][row_bomb] = MISS
-			grid_defend[col_bomb][row_bomb] = MISS 
+	if hit == "False":
+			grid_attack[row_bomb][col_bomb] = MISS
+			grid_defend[row_bomb][col_bomb] = MISS 
 			print('MISS')
-	print(grid_defend)
-	print(grid_attack)
+			
 	
 
 #check to see if there is any ship left on the grid, if no ship left return True as sunk else return false						
-def all_ships_sunk(grid_defend):
-	# TEMP VARIABLES
-	check_column = 0
-	check_row =0
-	Sunk= False
-	if direction == 'h'
-            for check_column in range(col,col+VESSEL_SIZE[index]):
-                if grid[check_column][check_row] != 5 or 4 or 3 or 2 :
-                    return True
-					
-	elif direction == 'v':
-            for check_y in range(row,row+GRID_HEIGHT):
-                if grid[check_column][check_row] != 5 or 4 or 3 or 2:
-                    return True:
+def all_vessels_sunk(grid):
 	
-	else:
-		sunk=False
-	
-	return sunk
+	for check_row in range(GRID_HEIGHT):
+		for check_column in range(GRID_WIDTH):
+			for i in range(len(VESSEL_SIZE)):
+				if grid[check_row][check_column] == VESSEL_SIZE[i]:
+					return True
+				
+
 
