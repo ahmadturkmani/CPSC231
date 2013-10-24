3a8177a9f0a4ef7607d2c1e99da19314f1200e7e
Aelbannan <Ahmed3lbannan@gmail.com>
2013-10-24 13:48:30 -0600

    Update grid.py

--- Battleship/Week-6/grid.py
+++ Battleship/Week-6/grid.py
@@ -22,8 +22,8 @@ def print_grid(grid):
 			print( '|' + str(row + 1) + '| ', end = '');
 		else:
 			print( '|0' + str(row + 1) + '| ', end = '');			
-		for column in range(0, GRID_WIDTH):
-			print(grid[column][row] + ' ', end = '');
+		for col in range(0, GRID_WIDTH):
+			print(grid[row][col] + ' ', end = '');
 		print('|');
 	print();
 # end		
@@ -32,10 +32,10 @@ def add_vessel(grid, index, row, col, direction):
 
 	if direction == 'h': 
 			for col in range(col, col + VESSEL_SIZE[index]):
-				grid[col][row] = str(VESSEL_SIZE[index]);
+				grid[row][col] = str(VESSEL_SIZE[index]);
 	elif direction == 'v':
 			for row in range(row, row + VESSEL_SIZE[index]):
-				grid[col][row] = str(VESSEL_SIZE[index]);
+				grid[row][col] = str(VESSEL_SIZE[index]);
 				
 	return grid
 	
@@ -46,31 +46,35 @@ def has_overlap(grid, index, row, col, direction):
 	
 	if direction == 'h': 
 			for check_x in range(check_x, check_x + VESSEL_SIZE[index]):
-				if grid[check_x][check_y] !='~':
+				if grid[check_y][check_x] !='~':
 						return True;
 					
 	elif direction == 'v':
 			for check_y in range(check_y, check_y + VESSEL_SIZE[index]):
-				if grid[check_x][check_y] !='~':
+				if grid[check_y][check_x] !='~':
 						return True;
 	
 	return False
+	
 # check the location picked by ai or human is a hit or a miss and print 
 #a grid with an 'X' for a hit and an 'o' for a miss	
-def drop_bomb(grid_defend,grid_attack,row_bomb,col_bomb):
+def drop_bomb(grid_defend, grid_attack, row, col):
+
 	#if the location picked to bomb equal any of the VESSEL_SIZE its is a hit
-	hit = "False"
-	for index in range(len(VESSEL_SIZE)):
-		if grid_defend[row_bomb][col_bomb] == VESSEL_SIZE[index]:
-			grid_attack[row_bomb][col_bomb] = HIT
-			grid_defend[row_bomb][col_bomb] = HIT
-			print("hit")
-			hit = "True"
+	hit = False
+	for i in range(len(VESSEL_SIZE)):
+		
+		if grid_defend[row][col] == str(VESSEL_SIZE[i]):
+			
+			grid_attack[row][col] = HIT
+			grid_defend[row][col] = HIT
+			print("HIT!")
+			hit = True
 				
-	#the location is empty 	
-	if hit == "False":
-			grid_attack[row_bomb][col_bomb] = MISS
-			grid_defend[row_bomb][col_bomb] = MISS 
+	
+	if hit == False:
+			grid_attack[row][col] = MISS
+			grid_defend[row][col] = MISS 
 			print('MISS')
 			
 	
@@ -78,10 +82,10 @@ def drop_bomb(grid_defend,grid_attack,row_bomb,col_bomb):
 #check to see if there is any ship left on the grid, if no ship left return True as sunk else return false						
 def all_vessels_sunk(grid):
 	
-	for check_row in range(GRID_HEIGHT):
-		for check_column in range(GRID_WIDTH):
+	for row in range(GRID_HEIGHT):
+		for col in range(GRID_WIDTH):
 			for i in range(len(VESSEL_SIZE)):
-				if grid[check_row][check_column] == VESSEL_SIZE[i]:
+				if grid[row][col] == VESSEL_SIZE[i]:
 					return True
 				
 
