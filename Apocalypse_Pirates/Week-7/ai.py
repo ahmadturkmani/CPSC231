0104f11d05326b582b6ec3164df1b4121fa65c9b
Hilmi A. <2ndgenairborne@gmail.com>
2013-10-28 16:54:51 -0600

    Update ai.py

--- Apocalypse_Pirates/Week-7/ai.py
+++ Apocalypse_Pirates/Week-7/ai.py
@@ -6,13 +6,13 @@ def get_move():
 	col = random.randint(0,4)
 	row = random.randint(0,4)
 	
-		# Checks if user's location is on the board 
+		# Checks if ai's location is on the board 
 	if 0 <= row < grid.GRID_HEIGHT and 0 <= col < grid.GRID_WIDTH:
 
 		# Checks if chosen location isn't blank
 		if grid.grid[grid.List2Dto1D(row, col)] != grid.b:
 
-			#If space has either a white pawn or white knight
+			#If space has either a black pawn or black knight
 			if grid.grid[grid.List2Dto1D(row, col)] == 'BK' or grid.grid[grid.List2Dto1D(row, col)] == 'BP':
 
 				# ask where the user wants to place this piece
@@ -31,17 +31,23 @@ def get_move():
 		get_move()
 
 def get_endmove(row, col, piece):
+	
 	if piece == "BK":
 		coor = random.randint(0,7)
+		#Beginning the random initialization for a move. 
 		new_row = grid.KNIGHT_MOVE[coor][1] + row
 		new_col = grid.KNIGHT_MOVE[coor][0] + col
 		
+	#Only posibilities for a pawn move. 
 	elif piece == "BP":
 		new_row = row + 1
 		new_col = random.randint(col-1, col+1)	
 	else:
 		get_move()
+	
+	#Debugging
 	print(row, col, new_row, new_col, piece)
+	
 	if 0 <= new_row < grid.GRID_HEIGHT and 0 <= new_col < grid.GRID_WIDTH:
 		
 		# if move is valid, then move the piece
@@ -63,16 +69,17 @@ def get_endmove(row, col, piece):
 		
 		
 ## Checks if the move is valid according to piece type ####	
+##This needs BETTER FORMATTING, but it does the job. ##
 def validate_move(row, col, new_row, new_col, piece):
 
-	#If piece selected is a white knight
+	#If piece selected is a black knight
 	if piece == 'BK':
 
 		# We check if the knight is moving in an L-shape, and if the spot is either empty or has an enemy piece
 		for i in range(8):	
 			if (new_col == col + grid.KNIGHT_MOVE[i][0]) and (new_row == row + grid.KNIGHT_MOVE[i][1]) and ((grid.grid[grid.List2Dto1D(new_row, new_col)] == 'WK') or (grid.grid[grid.List2Dto1D(new_row, new_col)] == 'WP') or (grid.grid[grid.List2Dto1D(new_row, new_col)] == grid.b)):
 				
-				# if enemy is killed, taunt them!
+				# if enemy is killed, taunt them! Makes AI a little smarter
 				if (grid.grid[grid.List2Dto1D(new_row, new_col)] == 'WK' or grid.grid[grid.List2Dto1D(new_row, new_col)] == 'WP'): 
 					return True
 	#If piece is a Black pawn
