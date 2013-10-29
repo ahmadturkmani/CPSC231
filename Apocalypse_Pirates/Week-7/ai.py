d7f511e87a72d13679d79de5058f5f90eb42bfad
Aelbannan <Ahmed3lbannan@gmail.com>
2013-10-29 11:47:56 -0600

    Update ai.py

--- Apocalypse_Pirates/Week-7/ai.py
+++ Apocalypse_Pirates/Week-7/ai.py
@@ -1,7 +1,12 @@
 import grid
 import random
+import __main__
+
+
 
 def get_move():
+	
+	board = __main__.board	
 	
 	col = random.randint(0,4)
 	row = random.randint(0,4)
@@ -10,13 +15,13 @@ def get_move():
 	if 0 <= row < grid.GRID_HEIGHT and 0 <= col < grid.GRID_WIDTH:
 
 		# Checks if chosen location isn't blank
-		if grid.grid[grid.List2Dto1D(row, col)] != grid.b:
+		if board[grid.List2Dto1D(row, col)] != grid.b:
 
 			#If space has either a black pawn or black knight
-			if grid.grid[grid.List2Dto1D(row, col)] == 'BK' or grid.grid[grid.List2Dto1D(row, col)] == 'BP':
+			if board[grid.List2Dto1D(row, col)] == 'BK' or board[grid.List2Dto1D(row, col)] == 'BP':
 
 				# ask where the user wants to place this piece
-				get_endmove(row, col, grid.grid[grid.List2Dto1D(row, col)])
+				get_endmove(row, col, board[grid.List2Dto1D(row, col)], board)
 
 			# dont try stealin other pieces
 			else:
@@ -30,7 +35,7 @@ def get_move():
 	else:
 		get_move()
 
-def get_endmove(row, col, piece):
+def get_endmove(row, col, piece, board):
 	
 	if piece == "BK":
 		coor = random.randint(0,7)
@@ -51,11 +56,11 @@ def get_endmove(row, col, piece):
 	if 0 <= new_row < grid.GRID_HEIGHT and 0 <= new_col < grid.GRID_WIDTH:
 		
 		# if move is valid, then move the piece
-		if validate_move(row, col, new_row, new_col, piece):
-			grid.grid[grid.List2Dto1D(new_row, new_col)] = piece
-			grid.grid[grid.List2Dto1D(row, col)] = grid.b
+		if validate_move(row, col, new_row, new_col, piece, board):
+			board[grid.List2Dto1D(new_row, new_col)] = piece
+			board[grid.List2Dto1D(row, col)] = grid.b
 			print('Moved ' + piece + ' from ' + chr(col + ord('a')).upper() + str(row) + ' to ' + chr(new_col + ord('a')).upper() + str(new_row) + '.\n' )
-			grid.print_board()
+			grid.print_board(board)
 		
 		# if not valid, tell the player. Then ask again where they want to move
 		else:
@@ -65,35 +70,35 @@ def get_endmove(row, col, piece):
 	# same as above		
 	else:
 		print('Not on the board!\n')
-		get_endmove(row, col, piece)
+		get_endmove(row, col, piece, board)
 		
 		
 ## Checks if the move is valid according to piece type ####	
 ##This needs BETTER FORMATTING, but it does the job. ##
-def validate_move(row, col, new_row, new_col, piece):
+def validate_move(row, col, new_row, new_col, piece, board):
 
 	#If piece selected is a black knight
 	if piece == 'BK':
 
 		# We check if the knight is moving in an L-shape, and if the spot is either empty or has an enemy piece
 		for i in range(8):	
-			if (new_col == col + grid.KNIGHT_MOVE[i][0]) and (new_row == row + grid.KNIGHT_MOVE[i][1]) and ((grid.grid[grid.List2Dto1D(new_row, new_col)] == 'WK') or (grid.grid[grid.List2Dto1D(new_row, new_col)] == 'WP') or (grid.grid[grid.List2Dto1D(new_row, new_col)] == grid.b)):
+			if (new_col == col + grid.KNIGHT_MOVE[i][0]) and (new_row == row + grid.KNIGHT_MOVE[i][1]) and ((board[grid.List2Dto1D(new_row, new_col)] == 'WK') or (board[grid.List2Dto1D(new_row, new_col)] == 'WP') or (board[grid.List2Dto1D(new_row, new_col)] == grid.b)):
 				
 				# if enemy is killed, taunt them! Makes AI a little smarter
-				if (grid.grid[grid.List2Dto1D(new_row, new_col)] == 'WK' or grid.grid[grid.List2Dto1D(new_row, new_col)] == 'WP'): 
+				if (board[grid.List2Dto1D(new_row, new_col)] == 'WK' or board[grid.List2Dto1D(new_row, new_col)] == 'WP'): 
 					return True
 	#If piece is a Black pawn
 	elif piece == 'BP':
 
 		# If there's nothing in front of the piece, move it up 
-		if (new_col == col) and (new_row == row + 1) and (grid.grid[grid.List2Dto1D(new_row, new_col)] == grid.b):
+		if (new_col == col) and (new_row == row + 1) and (board[grid.List2Dto1D(new_row, new_col)] == grid.b):
 			return True
 
 	        # If there is an enemy to the top right, kill it 
-		elif (new_col == col + 1) and (new_row == row + 1) and ((grid.grid[grid.List2Dto1D(new_row, new_col)] == 'WK') or (grid.grid[grid.List2Dto1D(new_row, new_col)] == 'WP')):
+		elif (new_col == col + 1) and (new_row == row + 1) and ((board[grid.List2Dto1D(new_row, new_col)] == 'WK') or (board[grid.List2Dto1D(new_row, new_col)] == 'WP')):
 			return True
 
 		# If there is an enemy to the top left, kill it 
-		elif (new_col == col - 1) and (new_row == row + 1) and ((grid.grid[grid.List2Dto1D(new_row, new_col)] == 'WK') or (grid.grid[grid.List2Dto1D(new_row, new_col)] == 'WP')):
+		elif (new_col == col - 1) and (new_row == row + 1) and ((board[grid.List2Dto1D(new_row, new_col)] == 'WK') or (board[grid.List2Dto1D(new_row, new_col)] == 'WP')):
 			return True
 		
