e518d52e03b39fe54bd8f97c3e715633d8fde19a
Aelbannan <Ahmed3lbannan@gmail.com>
2013-10-29 11:49:32 -0600

    Update main.py

--- Apocalypse_Pirates/Week-7/main.py
+++ Apocalypse_Pirates/Week-7/main.py
@@ -5,6 +5,7 @@ import grid
 import human
 import ai
 
+board = [grid.b for i in range(grid.GRID_WIDTH * grid.GRID_HEIGHT)]
 
 def show_title():#Printing initialziation - intro screen
 
@@ -19,40 +20,20 @@ def show_title():#Printing initialziation - intro screen
         print('~~~~~~~~\Created by the Apocalyptic Pirates/~~~~~~~')
         print('~~~~~~~~~\________copyright 2013(c)_______/~~~~~~~~')
         print()
-
-
-
-		
-		
-
-def get_winner(): #Checks if AI, or Human has won, prints trophy if human wins, otherwise prints you lose. 
-	black_alive = False
-	white_alive = False
-	for i in range(grid.GRID_HEIGHT * grid.GRID_WIDTH): #Running loop a total of 5x5 times; 25
-		if grid.grid[i] == "BP" or grid.grid[i] ==  "BK": 
-			black_alive = True
-		if grid.grid[i] == "WP" or grid.grid[i] == "WK":
-			white_alive = True
-	if white_alive == True and black_alive == True:		
-		return True
-	elif white_alive == True and black_alive == False:
-		print(	'/---------------------------\\n|----------YOU WIN!----------|\n==============================\n| | | | | | | | | | | | | | ||\n******************************\n             | |                \n       ==============')
-		return False
-	elif black_alive == True and white_alive == False:
-		print("You Lose.")
-		return False
 		
 
 def main():
+	global board
 	#Initialization
 	# add pieces to board and print board. 
-	grid.setup_board()
-	grid.print_board()
+	board = grid.setup_board(board)
+	grid.print_board(board)
 
 	# game loop. Play till user decides to quit
-	while get_winner():
-		grid.check_knight()
-		human.get_choice()
+	while grid.get_winner(board):
+		
+		human.get_choice() 
 		ai.get_move()
+		board = grid.check_knight(board)
 			
 main()
