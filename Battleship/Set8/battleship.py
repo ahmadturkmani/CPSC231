7e2a608795735d1cb4e871f732196245cbec0a4f
Hilmi A. <2ndgenairborne@gmail.com>
2013-11-14 09:59:31 -0700

    Create battleship.py

---
+++ Battleship/Set8/battleship.py
@@ -0,0 +1,15 @@
+###### Week 8 exercise 1 ################
+# (unchanged for this exercise)
+
+import t00_0_ai
+import grid
+import guia
+
+
+            
+
+def main() :
+        GUI = guia.BattleshipGUI()
+
+
+main()
