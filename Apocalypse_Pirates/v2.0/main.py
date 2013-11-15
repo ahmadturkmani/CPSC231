000ca122780970441df3533b08594c387ae00bd2
Hilmi A. <2ndgenairborne@gmail.com>
2013-11-15 12:50:26 -0700

    Update main.py

--- Apocalypse_Pirates/v2.0/main.py
+++ Apocalypse_Pirates/v2.0/main.py
@@ -3,3 +3,32 @@
 #This will contain the main game loop.
 #T2G5 Apocalypse Project - main.py:
 
+#Imports
+import sys
+import human
+import ai
+import grid
+
+def title_screen():
+  #Print Titlescreen
+  print("Welcome to T2G5's Apocalypse Game")
+  
+
+def __main__():
+  #Initialization Loop
+  human.get_choice()
+  while initialization is False:
+    grid.initialize()
+  
+  #Game Loop 
+  while get_winner() is False:
+    human.get_choice()
+    ai.get_choice()
+    grid.make_move()
+  
+  if get_winner() is True:
+    display_winner()
+    save_state()
+    sys.exit()
+
+#End main.py 
