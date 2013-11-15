56464675ff47a5a8298ab237837767485d7b31b6
Hilmi A. <2ndgenairborne@gmail.com>
2013-11-15 12:43:53 -0700

    Create main.py

---
+++ Apocalypse_Pirates/v2.0/main.py
@@ -0,0 +1,5 @@
+#V2.0 of this program will eventually contain a working version of apocalpyse playable
+#to the spec. documented in the CPSC231 website. 
+#This will contain the main game loop.
+#T2G5 Apocalypse Project - main.py:
+
