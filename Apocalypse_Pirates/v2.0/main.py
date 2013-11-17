8fb1876bf372672a97efd0b7d31d5afea00ee5b8
Hilmi A. <2ndgenairborne@gmail.com>
2013-11-16 22:27:39 -0700

    Update main.py

--- Apocalypse_Pirates/v2.0/main.py
+++ Apocalypse_Pirates/v2.0/main.py
@@ -1,34 +1,9 @@
-#V2.0 of this program will eventually contain a working version of apocalpyse playable
+#V2.0 of this program will eventually contain a working version of Apocalypse playable
 #to the spec. documented in the CPSC231 website. 
 #This will contain the main game loop.
 #T2G5 Apocalypse Project - main.py:
 
 #Imports
-import sys
-import human
-import ai
-import grid
+import gui
 
-def title_screen():
-  #Print Titlescreen
-  print("Welcome to T2G5's Apocalypse Game")
-  
-
-def __main__():
-  #Initialization Loop
-  human.get_choice()
-  while initialization is False:
-    grid.initialize()
-  
-  #Game Loop 
-  while get_winner() is False:
-    human.get_choice()
-    ai.get_choice()
-    grid.make_move()
-  
-  if get_winner() is True:
-    display_winner()
-    save_state()
-    sys.exit()
-
-#End main.py 
+GUI = gui.ApocalypseGUI()
