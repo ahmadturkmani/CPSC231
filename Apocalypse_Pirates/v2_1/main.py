e57763cb571406a276a1f3871c58021f03b3269a
Aelbannan <Ahmed3lbannan@gmail.com>
2013-11-20 18:06:31 -0700

    Create main.py

---
+++ Apocalypse_Pirates/v2_1/main.py
@@ -0,0 +1,10 @@
+#V2.0 of this program will eventually contain a working version of Apocalypse playable
+#to the spec. documented in the CPSC231 website. 
+#This will contain the main game loop.
+#T2G5 Apocalypse Project - main.py:
+
+#Imports
+import gui
+import grid
+
+GUI = gui.ApocalypseGUI(960, 720)
