e12990e8548019f202bbe6128396eefa8e2820aa
FaceTheDream <2ndgenairborne@gmail.com>
2013-11-26 17:49:17 -0700

    Works proper.
    Its commented, it works well and its not complicated. To use, use the
    following syntax:
    
    audio.play_music("filename")

--- Apocalypse_Pirates/v2_1/audio.py
+++ Apocalypse_Pirates/v2_1/audio.py
@@ -1,6 +1,9 @@
-import pyglet
+import winsound
+import os
 
 def play_music(file):
-	music = pyglet.resource.media(file)
-	music.play()
-	pyglet.app.run()
+	script_dir = os.path.dirname(__file__) #Absolute directory the script is in
+	rel_path = 'resources/audio/' #The path that will never change, this is where the audio is located
+	abs_file_path = os.path.join(script_dir, rel_path, file) #This is combining the three strings together
+	winsound.PlaySound(abs_file_path, winsound.SND_FILENAME) #Playing the wave file(SND_FILENAME), and abs_file_path is the location of audio
+	
